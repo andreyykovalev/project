19f4aed16990c237d43aca714a45b32de049a283
Andrey Kovalev <andreyykovalev@gmail.com>
2018-02-09 10:48:20 +0200

    addded session invalidation class

--- src/main/java/com/epam/rd/controllers/SessionInvalidation.java
+++ src/main/java/com/epam/rd/controllers/SessionInvalidation.java
@@ -1,18 +1,18 @@
 package com.epam.rd.controllers;
 
 
-import com.epam.rd.model.ModelPackage;
+
 import com.epam.rd.util.LanguageDefiner;
 import com.epam.rd.util.LocaleMessageProvider;
 
-import javax.servlet.RequestDispatcher;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.List;
+
 
 public class SessionInvalidation extends HttpServlet {
 
